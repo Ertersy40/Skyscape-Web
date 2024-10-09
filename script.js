e82489e60ea9e262a266f53617c9986fb12eb0cc
Ertersy40 <wonk4040@gmail.com>
2024-10-09 13:57:57 +1100

    background image compressed to webp

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ var highResImage = new Image();
 // when medium res image has loaded, replace the low res image
 
 
-// start loading medium res image
-medResImage.src = "../Assets/Images/display-photo-med.jpg";
+// // start loading medium res image
+// medResImage.src = "../Assets/Images/display-photo-med.jpg";
 
-document.addEventListener('DOMContentLoaded', function() {
-    medResImage.onload = function() {
-        document.getElementById('mainBackgroundImg').src = medResImage.src;
-        document.getElementById('mainBackgroundImg').style.filter = "none";
-        // start loading high res image
-        highResImage.src = "../Assets/Images/display-photo-full.JPG";
-    }
+// document.addEventListener('DOMContentLoaded', function() {
+//     medResImage.onload = function() {
+//         document.getElementById('mainBackgroundImg').src = medResImage.src;
+//         document.getElementById('mainBackgroundImg').style.filter = "none";
+//         // start loading high res image
+//         highResImage.src = "../Assets/Images/display-photo-full.JPG";
+//     }
     
-    // when high res image has loaded, replace the medium res image
-    highResImage.onload = function() {
-        document.getElementById('mainBackgroundImg').src = highResImage.src;
-    }
-});
+//     // when high res image has loaded, replace the medium res image
+//     highResImage.onload = function() {
+//         document.getElementById('mainBackgroundImg').src = highResImage.src;
+//     }
+// });
