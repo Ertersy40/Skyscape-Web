1620248bdb511826ece92f1f7f92ba1f9becba92
Ertersy40 <wonk4040@gmail.com>
2024-10-09 13:51:23 +1100

    Moved pages into folders for cleaner urls

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ var highResImage = new Image();
 
 
 // start loading medium res image
-medResImage.src = "./Assets/Images/display-photo-med.jpg";
+medResImage.src = "../Assets/Images/display-photo-med.jpg";
 
 document.addEventListener('DOMContentLoaded', function() {
     medResImage.onload = function() {
         document.getElementById('mainBackgroundImg').src = medResImage.src;
         document.getElementById('mainBackgroundImg').style.filter = "none";
         // start loading high res image
-        highResImage.src = "./Assets/Images/display-photo-full.JPG";
+        highResImage.src = "../Assets/Images/display-photo-full.JPG";
     }
     
     // when high res image has loaded, replace the medium res image
